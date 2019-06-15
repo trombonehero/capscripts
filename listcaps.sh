baf59563266a351209d39bd7db6bf24997caec95
Jonathan Anderson <jonathan.anderson@ieee.org>
2019-06-15 15:56:54 -0230

    Add more 'not these caps' possibilities.

--- listcaps.sh
+++ listcaps.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-grep CAP_ */*.c */*/*.c \
-	| egrep -v "([A-Z_]CAP_)|(CAP_HAS)|(CAP_NEEDS)|(CAP_ABOVE)|(CAP_ISA)|(CAP_NET_ADMIN)" \
+NOT_THE_CAPS_YOURE_LOOKING_FOR="([A-Z0-9_]CAP_)|(CAP_HAS)|(CAP_NEEDS)|(CAP_ABOVE)|(CAP_ISA)|(CAP_NET_ADMIN)|(GETCAP)|(\!capable)|(AGP)|(PCI)"
+
+grep CAP_ */*.c */*/*.c */*/*/*.c */*/*/*/*.c \
+	| egrep -v "$NOT_THE_CAPS_YOURE_LOOKING_FOR" \
 	| sed 's/^.*CAP_\([A-Z_]\+\)[,; )?\.].*$/CAP_\1/' \
 	| uniq | sort | uniq
 
